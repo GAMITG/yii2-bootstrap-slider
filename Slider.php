d36e28f5128637fdc1dc1cf3895d02d9e1124ed2
Андрей Ждановских <andrew72ru@gmail.com>
2015-07-30 10:35:31 +0500

    Set values of slider

--- Slider.php
+++ Slider.php
@@ -5,6 +5,8 @@ namespace andrew72ru\slider;
 use andrew72ru\slider\assets\SliderAsset;
 use yii\helpers\ArrayHelper;
 use yii\helpers\Html;
+use yii\helpers\Json;
+use yii\helpers\VarDumper;
 use yii\widgets\InputWidget;
 
 /**
@@ -14,6 +16,9 @@ class Slider extends InputWidget
 {
     public $min;
     public $max;
+    public $value1;
+    public $value2;
+    public $step;
 
     public function init()
     {
@@ -24,6 +29,15 @@ class Slider extends InputWidget
         if($this->min == $this->max)
             $this->min = 0;
 
+        if(!$this->value1)
+            $this->value1 = $this->min;
+
+        if(!$this->value2)
+            $this->value2 = $this->max;
+
+        if(!$this->step)
+            $this->step = 1;
+
         SliderAsset::register($this->view);
         $this->view->registerCss(".slider.slider-horizontal{width: 100%;}");
         $this->options['class'] = ArrayHelper::getValue($this->options, 'class', 'form-control');
@@ -31,8 +45,8 @@ class Slider extends InputWidget
         $this->options['data'] = [
             'slider-min' => $this->min,
             'slider-max' => $this->max,
-            'slider-step' => 1,
-            'slider-value' => [(int)$this->min, (int)$this->max]
+            'slider-step' => $this->step,
+            'slider-value' => [(int)$this->value1, (int)$this->value2]
         ];
 
         $this->view->registerJs("$('#{$this->id}').slider({tooltip:'always', formatter: function(value){return value[0] + ' – ' + value[1];}});");
@@ -40,6 +54,7 @@ class Slider extends InputWidget
 
     public function run()
     {
+//        $this->view->registerJs("console.log(" . Json::encode(VarDumper::dumpAsString($this->value2)) . ")");
         return Html::tag('div', Html::textInput(Html::getInputName($this->model, $this->attribute), null, $this->options), ['class' => 'form-group']);
     }
 }
